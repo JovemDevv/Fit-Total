6da70aa06d3a594673717d35caf94639083d877e
JovemDevv <anacarolinesan18@gmail.com>
2023-09-11 22:48:03 -0300

    introduzindo o material UI

--- FitTotal-app/src/App.jsx
+++ FitTotal-app/src/App.jsx
@@ -1,7 +1,11 @@
 import './style/App.css'
 import Header from './components/Header'
-import Card from './components/Card'
-import { Fragment, useEffect, useState } from 'react'
+import CardCategories from './components/Card'
+import { useEffect, useState } from 'react'
+import { Grid, Typography, Container } from '@mui/material'
+
+
+
 const categorias = ['Proteínas', 'Termogênicos', 'Roupas e acessórios']
 
 function App() {
@@ -20,20 +24,20 @@ function App() {
   return (
     <>
       <Header query={query} setQuery={setQuery} />
-      <main>
-        <div className='container'>
-          <div className='text'>
-            <h1>Produtos Fit Total - Categorias</h1>
-          </div>
-          <div className='cards'>
-            {categories.map((item, index) => (
-              <Fragment key={index}>
-                <Card title={item} index={index} setClicked={setClicked} /> 
-              </Fragment>
+
+      <Container maxWidth={"xl"} sx={{mt:4}}>
+        <Typography variant='h4' color={"#3b4046"}>Produtos FitTotal - Categorias</Typography>
+
+        <Grid container spacing={2} mt={5}>
+          
+          {categories.map((item, index) => (
+            <Grid item xs={12} md={6} lg={4} xl={3} key={index}>
+              <CardCategories title={item} /> 
+            </Grid>
             ))}
-          </div>
-        </div>
-      </main>
+          
+        </Grid>
+      </Container>
     </>
   )
 }
--- FitTotal-app/src/components/Card.jsx
+++ FitTotal-app/src/components/Card.jsx
@@ -1,22 +1,45 @@
-// eslint-disable-next-line react/prop-types
-function Card({ title, index, setClicked }) {
-  function handleClick() {
-    setClicked(index);
-  }
+import { Typography, Card, Stack, Box, IconButton  } from '@mui/material'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 
+
+// eslint-disable-next-line react/prop-types
+function CardCategories({ title }) {
+  
   return (
-    <div className='card'>
-      <div className='cardContent'>
-        <div className='textContent'>
-          <h3>{title}</h3>
-          <p>10 produtos encontrados</p>
-        </div>
-        <div className='buttonIn'>
-          <button onClick={handleClick}>&gt;</button>
-        </div>
-      </div>
-    </div>
+    <Card 
+      variant="outlined" 
+      sx={{
+        backgroundColor: "#3b4046", 
+        height: "200px",
+        p: 4,
+        borderRadius: 3,
+      }}> 
+
+      <Stack direction={"row"}
+        alignItems={"center"} 
+        justifyContent={"space-between"}
+        sx={{ height: "100%" }}>
+          <Stack direction={"column"}>
+            <Typography variant="h5" color={"#ddda16"}>
+              { title }
+            </Typography>
+            <Typography variant="body2">
+              10 produtos encontrados
+              </Typography>
+          </Stack>
+          <Box sx={{
+            height:"40px",
+            width:"40px",
+            backgroundColor:"#ddda16be",
+            borderRadius:2,}}>
+              <IconButton sx={{color: "white"}}>
+                <ArrowForwardIosIcon/>
+              </IconButton>
+            </Box>
+        </Stack>
+              
+     </Card>
   );
 }
 
-export default Card;
+export default CardCategories;
